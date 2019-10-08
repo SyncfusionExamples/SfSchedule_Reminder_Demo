c6af2befab4e0047863c216f6bdfd690516e5be3
Mohanraj-Guna <45057763+Mohanraj-Guna@users.noreply.github.com>
2019-10-08 17:27:33 +0530

    Add files via upload

---
+++ ReminderDemo/ReminderDemo/Meeting.cs
@@ -0,0 +1,22 @@
+﻿using Syncfusion.UI.Xaml.Schedule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ReminderDemo
+{
+    /// <summary>
+    /// Represents custom data properties.
+    /// </summary>
+    public class Meeting
+    {
+        public String Subject { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public Brush AppointmentColor { get; set; }
+        public ReminderTimeType ReminderTime { get; set; }
+    }
+}
